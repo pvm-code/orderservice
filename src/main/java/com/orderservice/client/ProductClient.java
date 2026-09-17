0ae5d4f05fc71f690a62785cf6ed5206c7e1f9aa
prashant <pvm.code@gmail.com>
2026-09-17 13:58:21 +0530

    keycloak added

--- src/main/java/com/orderservice/client/ProductClient.java
+++ src/main/java/com/orderservice/client/ProductClient.java
@@ -6,24 +6,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 import com.orderservice.dto.client.ProductClientResponse;
-import com.orderservice.security.ServiceJwtService;
 
 @Component
 public class ProductClient {
-	
-	private final RestClient restClient;
-	
-	private final ServiceJwtService serviceJwtService;
 
-	public ProductClient(
-	        RestClient restClient,
-	        ServiceJwtService serviceJwtService) {
+    private final RestClient restClient;
 
-	    this.restClient = restClient;
-	    this.serviceJwtService = serviceJwtService;
-	}
-	
-	public ProductClientResponse getProduct(UUID productId) {
+    public ProductClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public ProductClientResponse getProduct(UUID productId) {
 
         return restClient
                 .get()
@@ -33,32 +26,28 @@ public class ProductClient {
                 .retrieve()
                 .body(ProductClientResponse.class);
     }
-	public void decreaseStock(UUID productId, int quantity) {
 
-	    String token = serviceJwtService.createProductServiceToken();
+    public void decreaseStock(UUID productId, int quantity) {
 
-	    restClient
-	            .put()
-	            .uri(uriBuilder -> uriBuilder
-	                    .path("/internal/inventory/{id}/reserve")
-	                    .queryParam("quantity", quantity)
-	                    .build(productId))
-	            .header("Authorization", "Bearer " + token)
-	            .retrieve()
-	            .toBodilessEntity();
-	}
-	public void increaseStock(UUID productId, int quantity) {
+        restClient
+                .put()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/internal/inventory/{id}/reserve")
+                        .queryParam("quantity", quantity)
+                        .build(productId))
+                .retrieve()
+                .toBodilessEntity();
+    }
 
-	    String token = serviceJwtService.createProductServiceToken();
+    public void increaseStock(UUID productId, int quantity) {
 
-	    restClient
-	            .put()
-	            .uri(uriBuilder -> uriBuilder
-	                    .path("/internal/inventory/{id}/release")
-	                    .queryParam("quantity", quantity)
-	                    .build(productId))
-	            .header("Authorization", "Bearer " + token)
-	            .retrieve()
-	            .toBodilessEntity();
-	}
+        restClient
+                .put()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/internal/inventory/{id}/release")
+                        .queryParam("quantity", quantity)
+                        .build(productId))
+                .retrieve()
+                .toBodilessEntity();
+    }
 }
