1e2eefbeb9a0be9d89b9a7529496edf2e6284e72
prashant <pvm.code@gmail.com>
2026-09-24 18:59:17 +0530

    internal service

--- src/main/java/com/orderservice/config/SecurityConfig.java
+++ src/main/java/com/orderservice/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -58,6 +59,7 @@ public class SecurityConfig {
      * ============================================================
      */
     @Bean
+    @Primary
     @Order(1)
     public SecurityFilterChain internalSecurityFilterChain(
             HttpSecurity http,
